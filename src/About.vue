041518cb6b19f5ec4f1c178a35d2d5a424d97439
flodavis <fpdavis86@gmail.com>
2019-03-15 09:19:57 -0400

    Added defaults for vue-static

---
+++ src/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <ul>
+            <li>About Me</li>
+            <li>About Us</li>
+            <li>About Our History</li>
+        </ul>
+    </div>
+</template>
